34c03454f74c9f3a714679a48469b47529565739
Kal Zheng <kal.zheng@inspire-dt.com>
2023-06-09 14:28:04 +0800

    add login and register check for error

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import { login } from "../util/auth";
@@ -9,7 +10,14 @@ function LoginScreen() {
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication Failed!",
+        "Could not log you in. Please check your credential or try again later"
+      );
+    }
     setIsAuthenticating(false);
   }
 
